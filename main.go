6f4ba68b67be64e3ca3b49be5629f7223d0964f8
xy <xyzmhx@gmail.com>
2023-07-09 07:07:58 +0900

    rewrite handler

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// Package main provides the main application to run the Discord bot
-// which interacts with the GPT API
 package main
 
 import (
@@ -9,107 +7,97 @@ import (
 	"strings"
 	"syscall"
 	"time"
-	"xy.com/discordbot/bard"
 
 	"github.com/bwmarrin/discordgo"
+	"xy.com/discordbot/bard"
 	"xy.com/discordbot/c2gptapi"
 )
 
-// main initializes the Discord bot, sets up the event handlers, and starts the bot
 func main() {
-	// Get the Discord bot token from the environment variable
 	token := os.Getenv("DISCORD_BOT_TOKEN")
 	if token == "" {
 		fmt.Println("Error: DISCORD_BOT_TOKEN environment variable not set.")
 		return
 	}
 
-	// Create a new Discord session with the given bot token
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("Error creating Discord session:", err)
 		return
 	}
 
-	// Add the message create event handler to the Discord session
 	dg.AddHandler(messageCreate)
 
-	// Open a WebSocket connection to Discord
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("Error opening Discord session:", err)
 		return
 	}
 
-	// Notify the user that the bot is running and waiting for messages
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	// Wait for a termination signal (CTRL-C)
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
-	// Close the Discord session
 	dg.Close()
 }
 
-// messageCreate is called every time a new message is created on any channel
-// that the authenticated bot has access to
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore messages sent by the bot itself
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// Process commands starting with "."
+	msg, _ := s.ChannelMessageSend(m.ChannelID, "Processing...")
+
 	if strings.HasPrefix(m.Content, ".") {
-		search := m.Content[1:]
-		output := make(chan string)
-		var msg *discordgo.Message
-		builder := &strings.Builder{}
+		handleGPTCommand(s, m, msg)
+	} else if strings.HasPrefix(m.Content, "!") {
+		handleTextResponseCommand(s, m, msg)
+	} else if strings.HasPrefix(m.Content, "~") {
+		handleChatResponseCommand(s, m, msg)
+	}
+}
 
-		// Send an initial message to indicate the bot is processing the command
-		msg, _ = s.ChannelMessageSend(m.ChannelID, "Processing...")
+func handleGPTCommand(s *discordgo.Session, m *discordgo.MessageCreate, msg *discordgo.Message) {
+	search := m.Content[1:]
+	output := make(chan string)
+	builder := &strings.Builder{}
 
-		// Start a new Goroutine to chat with the GPT API
-		go c2gptapi.ChatWithGPT(search, output)
-		ticker := time.NewTicker(800 * time.Millisecond)
+	go c2gptapi.ChatWithGPT(search, output)
+	ticker := time.NewTicker(800 * time.Millisecond)
 
-		// Update the bot's message with the GPT API's response
-		for {
-			select {
-			case value, ok := <-output:
-				if !ok {
-					ticker.Stop()
-					_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, builder.String())
-					return
-				}
-				builder.WriteString(value)
-			case <-ticker.C:
+	for {
+		select {
+		case value, ok := <-output:
+			if !ok {
+				ticker.Stop()
 				_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, builder.String())
+				return
 			}
+			builder.WriteString(value)
+		case <-ticker.C:
+			_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, builder.String())
 		}
 	}
+}
 
-	if strings.HasPrefix(m.Content, "!") {
-		msg, _ := s.ChannelMessageSend(m.ChannelID, "Processing...")
-		search := m.Content[1:]
-		output, err := bard.GenerateTextResponse(search)
-		if err != nil {
-			_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, fmt.Sprint(err))
-		} else {
-			_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, output)
-		}
+func handleTextResponseCommand(s *discordgo.Session, m *discordgo.MessageCreate, msg *discordgo.Message) {
+	search := m.Content[1:]
+	output, err := bard.GenerateTextResponse(search)
+	if err != nil {
+		_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, fmt.Sprint(err))
+	} else {
+		_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, output)
 	}
+}
 
-	if strings.HasPrefix(m.Content, "~") {
-		msg, _ := s.ChannelMessageSend(m.ChannelID, "Processing...")
-		search := m.Content[1:]
-		output, err := bard.GenerateChatResponse(search)
-		if err != nil {
-			_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, fmt.Sprint(err))
-		} else {
-			_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, output)
-		}
-
+func handleChatResponseCommand(s *discordgo.Session, m *discordgo.MessageCreate, msg *discordgo.Message) {
+	search := m.Content[1:]
+	output, err := bard.GenerateChatResponse(search)
+	if err != nil {
+		_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, fmt.Sprint(err))
+	} else {
+		_, _ = s.ChannelMessageEdit(msg.ChannelID, msg.ID, output)
 	}
 }
